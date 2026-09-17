e63bfc5fe352d6108f1730ec8646114d167b79c7
Leokakao-cyber <leoose6@gmail.com>
2026-09-17 18:13:57 +0100

    Add Lumen Android app/build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.lumen.keyboard"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.lumen.keyboard"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 3
+        versionName = "0.3.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
+
