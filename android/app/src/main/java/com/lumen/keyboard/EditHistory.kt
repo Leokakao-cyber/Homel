04bd47fd6d4a8a553c47a3c769405d6572dce310
Leokakao-cyber <leoose6@gmail.com>
2026-09-17 18:14:27 +0100

    Add Lumen Android app/src/main/java/com/lumen/keyboard/EditHistory.kt

---
+++ android/app/src/main/java/com/lumen/keyboard/EditHistory.kt
@@ -0,0 +1,25 @@
+package com.lumen.keyboard
+
+data class LumenEdit(val inserted: String, val removed: String = "")
+
+class EditHistory(private val capacity: Int = 5) {
+    private val undo = ArrayDeque<LumenEdit>()
+    private val redo = ArrayDeque<LumenEdit>()
+
+    fun record(edit: LumenEdit) {
+        undo.addLast(edit)
+        while (undo.size > capacity) undo.removeFirst()
+        redo.clear()
+    }
+
+    fun takeUndo(): LumenEdit? = undo.removeLastOrNull()?.also {
+        redo.addLast(it)
+        while (redo.size > capacity) redo.removeFirst()
+    }
+
+    fun takeRedo(): LumenEdit? = redo.removeLastOrNull()?.also {
+        undo.addLast(it)
+        while (undo.size > capacity) undo.removeFirst()
+    }
+}
+
